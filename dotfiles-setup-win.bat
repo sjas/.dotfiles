e804687fdfa489e2b8443e34df9ba45ded93d8e5
sjas <teilzeittresor@web.de>
2013-02-22 15:33:30 +0000

    batch setupfile also copies bash configs, since on win they are used, too

--- dotfiles-setup-win.bat
+++ dotfiles-setup-win.bat
@@ -2,5 +2,7 @@ cd %home%
 cp .dotfiles\vim\.vimrc _vimrc
 cp .dotfiles\git\.gitconfig .gitconfig
 cp -R .dotfiles/vim/.vim .
+cp .dotfiles\bash\.bashrc .bashrc
+cp .dotfiles\bash\.bash_profile .bash_profile
 vim +":source $MYVIMRC" +":q"
 cd .dotfiles
