96a7e4adbaaf259f122a5266d03a778c948976da
sjas <teilzeittresor@web.de>
2014-12-04 21:24:09 +0100

    autodeploy vundle and zsh

--- deploy-zsh-and-vundle.sh
+++ deploy-zsh-and-vundle.sh
@@ -3,4 +3,8 @@
 git clone https://github.com/sjas/.oh-my-zsh ~/.oh-my-zsh
 git clone https://github.com/sjas/zsh-syntax-highlighting ~/.zsh-syntax-highlighting
 
+git clone https://github.com/gmarik/Vundle ~/.vim/bundle/Vundle
+
 sudo vipw
+
+
