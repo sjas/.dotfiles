43fb087fd58b7c8dfec5f0f110cef9e494b93ca6
sjas <teilzeittresor@web.de>
2017-12-03 04:29:51 +0100

    terminator config added

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 grep -q '. /home/sjas/.dotfiles/.bashrc_main' /home/sjas/.bashrc || echo '. /home/sjas/.dotfiles/.bashrc_main' >> /home/sjas/.bashrc
 [ ! -L /home/sjas/.vimrc ] && ln -s /home/sjas/.dotfiles/.vimrc /home/sjas/.vimrc
+[ ! -L /home/sjas/.config/terminator ] && mkdir -p /home/sjas/.config && ln -s /home/sjas/.dotfiles/.config/terminator /home/sjas/.config/terminator
