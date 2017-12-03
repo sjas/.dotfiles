a467ce0fdca9f35a090d4dc572eacc7804c128dc
sjas <teilzeittresor@web.de>
2017-12-03 10:38:06 +0100

    commenting

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# automatically provisioning 
+
 sudo apt update
 sudo apt install colordiff git bash-completion whois -y
+
 grep -q '. /home/sjas/.dotfiles/.bashrc_main' /home/sjas/.bashrc || echo '. /home/sjas/.dotfiles/.bashrc_main' >> /home/sjas/.bashrc
 [ -L /home/sjas/.vimrc ] || ln -s /home/sjas/.dotfiles/.vimrc /home/sjas/.vimrc
 [ -L /home/sjas/.config/terminator/config ] || mkdir -p /home/sjas/.config/terminator && ln -s /home/sjas/.dotfiles/.config/terminator/config /home/sjas/.config/terminator/config
